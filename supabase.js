7c711fb8c328a5b265e52c7cb883299cd817deaa
glambysabina <glambysabina@yahoo.com>
2026-05-12 15:33:34 -0600

    supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
-const SUPABASE_URL = window.SUPABASE_URL || "YOUR_SUPABASE_URL";
-const SUPABASE_ANON_KEY = window.SUPABASE_ANON_KEY || "YOUR_SUPABASE_ANON_KEY";
+const SUPABASE_URL = window.SUPABASE_URL || "https://jdmzhqneamuzcfnzdyui.supabase.co";
+const SUPABASE_ANON_KEY = window.SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpkbXpocW5lYW11emNmbnpkeXVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2MDkyMjcsImV4cCI6MjA5NDE4NTIyN30.4H5JshIUxWFncg-Gicx-qbOcwDBZcLKfFIGDjuVquHs";
 
 const supabaseClient =
-  SUPABASE_URL.startsWith("http") && SUPABASE_ANON_KEY !== "YOUR_SUPABASE_ANON_KEY"
+  SUPABASE_URL.startsWith("http") && SUPABASE_ANON_KEY !== "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpkbXpocW5lYW11emNmbnpkeXVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2MDkyMjcsImV4cCI6MjA5NDE4NTIyN30.4H5JshIUxWFncg-Gicx-qbOcwDBZcLKfFIGDjuVquHs"
     ? window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
     : null;
